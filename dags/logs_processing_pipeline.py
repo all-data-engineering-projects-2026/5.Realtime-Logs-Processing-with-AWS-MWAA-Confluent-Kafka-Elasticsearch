76da0be580335143477e6a45b2b6eb59fec55de5
Himanshu Patidar <himanshu@Himanshus-MacBook-Pro.local>
2026-06-22 19:26:12 +0530

    mwaa project setup

---
+++ dags/logs_processing_pipeline.py
@@ -0,0 +1,156 @@
+import re
+from datetime import datetime, timedelta
+import json
+import logging
+
+import boto3
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from botocore.exceptions import ClientError
+from confluent_kafka import Consumer, KafkaException
+from elasticsearch import Elasticsearch, helpers
+from elasticsearch.helpers import bulk
+
+# from dags.utils import get_secret
+
+logger = logging.getLogger(__name__)
+
+def get_secret(secret_name, region_name='us-east-1'):
+    """Retrieve secrets from AWS secret Manager"""
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        return json.loads(response['SecretString'])
+    except ClientError as e:
+        logger.error(f"Secret retrieval error : {e}")
+        raise
+
+def parse_log_entry(log_entry):
+    log_pattern = r'(?P<ip>[\d.]+) - - \[(?P<timestamp>.*)\] "(?P<method>\w+) (?P<endpoint>[\w/]+) (?P<protocol>[\w/\.]+)'
+    match = re.match(log_pattern, log_entry)
+    if not match:
+        logger.warning(f"Invalid log format: {log_entry}")
+        return None
+
+    data = match.groupdict()
+
+    try:
+        parsed_timestamp = datetime.strptime(data['timestamp'], '%b %d %Y, %H:%M:%S')
+        data['@timestamp'] = parsed_timestamp.isoformat()
+    except ValueError:
+        logger.error(f"Timestamp parsing error: {data['timestamp']}")
+        return None
+
+    return data
+
+
+def consume_and_index_logs(**context):
+    secrets = get_secret("MWAA_Secrets_V2")
+
+    consumer_config = {
+        'bootstrap.servers': secrets['KAFKA_BOOTSTRAP_SERVER'],
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': secrets['KAFKA_SASL_USERNAME'],
+        'sasl.password': secrets['KAFKA_SASL_PASSWORD'],
+        'group.id': 'mwaa_log_indexer',
+        'auto.offset.reset': 'latest'
+    }
+
+    es_config = {
+        'hosts': [secrets['ELASTICSEARCH_URL']],
+        'api_key': secrets['ELASTICSEARCH_API_KEY']
+    }
+
+    consumer = Consumer(consumer_config)
+    es = Elasticsearch(**es_config)
+    topic = 'billion_website_logic'
+    consumer.subscribe([topic])
+
+    try:
+        index_name = 'billion_website_logs'
+        if not es.indices.exists(index=index_name):
+            es.indices.create(index=index_name)
+            logger.info(f'Created index: {index_name}')
+    except Exception as e:
+        logger.error(f"Failed to create index:{index_name}")
+
+    try:
+        logs = []
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                break
+
+            if msg.error():
+                if msg.error().code() + + KafkaException._PARTITION_EOF:
+                    break
+                raise KafkaException(msg.error())
+
+            log_entry = msg.value().decode('utf-8')
+            parsed_log = parse_log_entry(log_entry)
+
+            if parsed_log:
+                logs.append(parsed_log)
+
+            # index when 15000 logs are collected
+            if len(logs) >= 15000:
+                actions = [
+                    {
+                        '_op_type': 'create',
+                        '_index': index_name,
+                        '_source': log
+                    }
+                    for log in logs
+                ]
+
+                success, failed = bulk(es, actions, refresh=True)
+                logger.info(f'Indexed {success} logs, {len(failed)} failed')
+                logs = []
+    except Exception as e:
+        logger.error(f"Failed to index log : {e}")
+
+    try:
+        # index and remaining logs
+        if logs:
+            actions = [
+                {
+                    '_op_type': 'create',
+                    '_index': index_name,
+                    '_source': log
+                }
+                for log in logs
+            ]
+            bulk(es, actions, refresh=True)
+    except Exception as e:
+        logger.error(f'Log processing error: {e}')
+    finally:
+        consumer.close()
+        es.close()
+
+
+default_args = {
+    'owner': 'himanshu_airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5),
+}
+
+dag = DAG(
+    'log_consumer_pipeline',
+    default_args=default_args,
+    description="Consume and Index synthetic logs",
+    schedule="*/5 * * * *",
+    start_date=datetime(2026, 6, 22),
+    catchup=False,
+    tags={'logs', 'kafka', 'production'}
+)
+
+consume_logs_task = PythonOperator(
+    task_id='generate_and_consume_logs',
+    python_callable=consume_and_index_logs,
+    dag=dag,
+)
+
---
+++ dags/logs_producer.py
@@ -0,0 +1,156 @@
+import json
+import random
+from datetime import timedelta, datetime
+
+import boto3
+from airflow import DAG
+from airflow.configuration import get_custom_secret_backend
+from airflow.providers.standard.operators.python import PythonOperator
+from botocore.exceptions import ClientError
+from confluent_kafka import Producer
+from faker import Faker
+import logging
+
+# from dags.utils import get_secret
+
+fake = Faker()
+logger = logging.getLogger(__name__)
+
+def get_secret(secret_name, region_name='us-east-1'):
+    """Retrieve secrets from AWS secret Manager"""
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        return json.loads(response['SecretString'])
+    except ClientError as e:
+        logger.error(f"Secret retrieval error : {e}")
+        raise
+
+def create_kafka_producer(config):
+    return Producer(config)
+
+
+def generate_log():
+    """Generate synthetic log"""
+    methods = ['GET', 'POST', 'PUT', 'DELETE']
+    endpoints = ['/api/users', '/home', '/about', '/contact', '/services']
+    statuses = [200, 301, 302, 400, 404, 500]
+
+    user_agents = [
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X)',
+        'Mozilla/5.0 (X11; Linux x86_64)',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)',
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    ]
+
+    referrers = ['https://www.google.com', 'https://example.com', '-', 'https://bing.com', 'https://yahoo.com']
+
+    ip = fake.ipv4()
+    timestamp = datetime.now().strftime('%b %d %Y, %H:%M:%S')
+    method = random.choice(methods)
+    endpoint = random.choice(endpoints)
+    status = random.choice(statuses)
+    size = random.randint(1000, 15000)
+    referrer = random.choice(referrers)
+    user_agent = random.choice(user_agents)
+
+    log_entry = (
+        f'{ip} - - [{timestamp}] {method} {endpoint} HTTP/1/1" {status} {size} {referrer} {user_agent}'
+    )
+
+    return log_entry
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        logger.error(f'Message delivery failed: {err}')
+    else:
+        logger.info(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+
+# TRY OUT FOR LOCAL
+# def get_secret(secret_name: str, region_name: str = 'us-east-1'):
+#     """Retrieve secrets from AWS Secrets Manager - works locally & in MWAA"""
+#     try:
+#         # Option 1: Use specific profile for local development
+#         session = boto3.session.Session(profile_name='mwaa-dev')
+#         logger.info("Running from actual credential - using mock secrets. Set real credentials for production.")
+#
+#         # Option 2: Fallback to default session (env vars or default profile)
+#         # session = boto3.session.Session()
+#
+#         client = session.client('secretsmanager', region_name=region_name)
+#
+#         response = client.get_secret_value(SecretId=secret_name)
+#         return json.loads(response['SecretString'])
+#
+#     except Exception as e:
+#         logger.warning(f"AWS credential/secret error: {e}")
+#
+#         # === LOCAL FALLBACK (for development) ===
+#         if "NoCredentialsError" in str(e) or "Credentials" in str(e):
+#             logger.info("Running locally - using mock secrets. Set real credentials for production.")
+#             # Mock secrets for local testing
+#             return {
+#                 "KAFKA_BOOTSTRAP_SERVER": "pkc-oxqxx9.us-east-1.aws.confluent.cloud:9092",
+#                 "KAFKA_SASL_USERNAME": "E6JIQDVYGPSPAR3T",
+#                 "KAFKA_SASL_PASSWORD": "cfltzMue3d97y6k4HkO702/aYmNLdESuf+xqrEW5WvX4Q6YGBZ0OdYyd2Q9yMORg"
+#             }
+#
+#         logger.error(f"Failed to retrieve secret {secret_name}")
+#         raise
+
+def produce_logs(**context):
+    """ Produce log entries into Kafka"""
+    secrets = get_secret("MWAA_Secrets_V2")
+    kafka_config = {
+        'bootstrap.servers': secrets['KAFKA_BOOTSTRAP_SERVER'],
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': secrets['KAFKA_SASL_USERNAME'],
+        'sasl.password': secrets['KAFKA_SASL_PASSWORD'],
+        'session.timeout.ms': 50000
+    }
+
+    producer = create_kafka_producer(kafka_config)
+    topic = 'billion_website_logs'
+
+    for _ in range(50):
+        log = generate_log()
+        try:
+            producer.produce(topic, log.encode('utf-8'), on_delivery=delivery_report)
+            producer.flush()
+        except Exception as e:
+            logger.error(f"Error producing log:{e}")
+            raise
+
+    logger.info(f"Produced 5000 logs to Kafka topic {topic}")
+
+
+default_args = {
+    'owner': 'himanshu_airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5),
+}
+
+dag = DAG(
+    'log_generation_pipeline',
+    default_args=default_args,
+    description="Generate and produce synthetic logs",
+    schedule="*/5 * * * *",
+    start_date=datetime(2026, 6, 22),
+    catchup=False,
+    tags={'logs', 'kafka', 'production'}
+)
+
+produce_logs_task = PythonOperator(
+    task_id='generate_and_produce_logs',
+    python_callable=produce_logs,
+    dag=dag,
+)
+
